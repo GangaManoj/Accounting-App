6676ebec227a309de38ef13c9d210591bb8e153e
Ganga Manoj <ganga.manoj98@gmail.com>
2020-11-30 20:53:28 +0530

    sales invoice test wip

--- accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.js
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.js
@@ -2,9 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Sales Invoice', {
-	links_add(frm, cdt, cdn){
-		validate(cdn){
-			cdn.amount = cdn.rate * cdn.quantity
-		}
-	}
+	// links_add(frm, cdt, cdn){
+	// 	validate(cdn){
+	// 		cdn.amount = cdn.rate * cdn.quantity
+	// 	}
+	// }
 });
--- accounting_app/accounting_app/doctype/sales_invoice/test_sales_invoice.py
+++ accounting_app/accounting_app/doctype/sales_invoice/test_sales_invoice.py
@@ -3,8 +3,28 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
 
 class TestSalesInvoice(unittest.TestCase):
-	pass
+	def tearDown(self):
+		frappe.db.rollback()
+
+	def test_ledger_entry_creation(self):
+		before_sales_invoice = frappe.db.count('Ledger Entry')
+		customer = frappe.get_doc("Customer", "Arjun Manoj")
+		company = frappe.get_doc("Company", "Gada Electronics")
+		doc = frappe.get_doc({
+			"doctype": "Sales Invoice",
+			"customer": customer,
+			"company": company,
+			"posting_date": "2020-11-20"
+		})
+		doc.append("items",{
+			"item": "xyz",
+			"quantity": 2
+		})
+		doc.insert()
+		after_sales_invoice = frappe.db.count('Ledger Entry')
+		difference = after_sales_invoice - before_sales_invoice
+		self.assertEqual(difference, 2, "Creation of sales invoice does not result in the creation of two corresponding ledger entries.")
