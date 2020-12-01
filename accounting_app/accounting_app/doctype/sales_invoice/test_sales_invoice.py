1f9ec640a849c561337f45e3c3b535b529866b9d
Ganga Manoj <ganga.manoj98@gmail.com>
2020-12-01 13:17:08 +0530

    commit

--- accounting_app/accounting_app/doctype/sales_invoice/test_sales_invoice.py
+++ accounting_app/accounting_app/doctype/sales_invoice/test_sales_invoice.py
@@ -12,19 +12,42 @@ class TestSalesInvoice(unittest.TestCase):
 
 	def test_ledger_entry_creation(self):
 		before_sales_invoice = frappe.db.count('Ledger Entry')
-		customer = frappe.get_doc("Customer", "Arjun Manoj")
-		company = frappe.get_doc("Company", "Gada Electronics")
 		doc = frappe.get_doc({
 			"doctype": "Sales Invoice",
-			"customer": customer,
-			"company": company,
-			"posting_date": "2020-11-20"
-		})
-		doc.append("items",{
-			"item": "xyz",
-			"quantity": 2
+			"customer": "Ganga Manoj",
+			"company": "Gada Electronics",
+			"posting_date": "2020-11-20",
+			"items" : [{
+				"item": "Moto x4",
+				"quantity": 2,
+			}]
 		})
 		doc.insert()
+		doc.submit()
 		after_sales_invoice = frappe.db.count('Ledger Entry')
 		difference = after_sales_invoice - before_sales_invoice
 		self.assertEqual(difference, 2, "Creation of sales invoice does not result in the creation of two corresponding ledger entries.")
+
+	def test_total(self):
+		doc = frappe.get_doc({
+			"doctype": "Sales Invoice",
+			"customer": "Ganga Manoj",
+			"company": "Gada Electronics",
+			"posting_date": "2020-11-20",
+			"items" : [{
+				"item": "Moto x4",
+				"quantity": 2,
+			},
+			{
+				"item": "Oneplus 7",
+				"quantity": 3,
+			}]
+		})
+		doc.insert()
+		total = 0
+		for item in doc.get('items'):
+			total += item.amount
+
+		self.assertEqual(doc.grand_total, total, "Grand Total is incorrect.")
+
+		
